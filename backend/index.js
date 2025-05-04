8fbb8c663223878a703205a966b101ef5e5e138f
ItsAJ1005 <ajharshvardhan123@gmail.com>
2025-05-04 13:16:55 +0530

    Update - CORS for deployement

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({extended:true}));
 const corsOptions = {
     origin: ['http://localhost:5173', "https://cubicles.netlify.app"],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma']
 }
 
